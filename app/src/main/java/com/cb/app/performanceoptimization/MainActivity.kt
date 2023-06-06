55a7153bea20602804627e9525da53450bc53151
[caobin] <[bincao2@oflytek.com]>
2023-06-06 17:52:05 +0800

    Initial commit

---
+++ app/src/main/java/com/cb/app/performanceoptimization/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cb.app.performanceoptimization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ plugin/src/main/java/com/cb/app/plugin/BoosterPlugin.kt
@@ -0,0 +1,12 @@
+package com.cb.app.plugin
+
+/**
+ * @author: bincao2
+ * @date: 2023/6/6 17:07
+ * @desc: 描述
+ * @updateUser: 更新者
+ * @updateDate: 2023/6/6 17:07
+ * @updateRemark: 更新说明
+ */
+class BoosterPlugin {
+}
